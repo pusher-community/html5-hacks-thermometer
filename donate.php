c94e2cf5332699cd86480c523a34111e24136107
Robin Hawkes <rob@rawkes.com>
2014-10-30 14:08:33 +0000

    MySQL fixes

--- donate.php
+++ donate.php
@@ -2,12 +2,7 @@
   require 'vendor/autoload.php';
   require('config.php');
 
-  $con = mysql_connect($db_host, $db_username, $db_password);
-    if (!$con)
-    {
-      die('Could not connect: ' . mysql_error());
-    }
-    mysql_select_db($db_name, $con);
+  $con = mysqli_connect($db_host, $db_username, $db_passwordk, $db_name) or die("Error " . mysqli_error($con));
   
   $who = $_GET['who'];  
   $who = mysql_real_escape_string( $who );  
@@ -19,24 +14,22 @@
   }
 
   $running_total = 0;
-  $last_update = "SELECT * FROM $db_tablename ORDER BY id DESC LIMIT 1";
-  $result = mysql_query($last_update);
+  $last_update = "SELECT * FROM $db_tablename ORDER BY id DESC LIMIT 1" or die("Error in the consult.." . mysqli_error($con);
+  $result = $con->query($last_update);
   if($result) {
-    $row = mysql_fetch_array($result);
+    $row = mysqli_fetch_array($result);
     $running_total = $row['running_total'];
   }
 
   $running_total = $running_total + $how_much;
   
   $insert_query = "INSERT INTO $db_tablename (who, how_much, running_total) ";
-  $insert_query .= sprintf( "VALUES('%s', %f, %f)", $who, $how_much, $running_total );
+  $insert_query .= sprintf( "VALUES('%s', %f, %f)", $who, $how_much, $running_total ) or die("Error in the consult.." . mysqli_error($con);
   
-  $insert_result = mysql_query($insert_query);
+  $insert_result = $con->query($insert_query);
   if(!$insert_result) {
     die('insert query failed' . mysql_error());
   }
-  
-  mysql_close($con);
   
   $pusher = new Pusher($pusher_key, $pusher_secret, $pusher_app_id);
   $channel_name = 'donations-channel';
